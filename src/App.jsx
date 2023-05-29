c53df41e6b5a06f733417c600abca1d29490d512
Budhijakuber23 <108067168+Budhijakuber23@users.noreply.github.com>
2023-05-29 15:34:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Homepage from "./homepage";
 import Threed from "./threed";
 import Skill from "./skill";
